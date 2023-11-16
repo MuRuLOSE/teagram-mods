b1ca254ace922388a8ca735f3f861bec2a02829e
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-11-16 12:31:51 +0300

    Create captcha.py

---
+++ captcha.py
@@ -0,0 +1,56 @@
+from googletrans import Translator
+
+from telethon.types import Message
+
+from .. import loader, utils
+
+#required: googletrans
+
+def set_default_lang(db):
+    
+    return db.get("teagram.loader", "lang", "ru")
+
+@loader.module(name="Translator", author="teagram_mods", version=1) 
+class TranslatorMod(loader.Module):                          
+    """Translate text"""
+
+    @loader.command(alias="tr")
+    async def translate(self, message: Message, args: str):
+        ''' - <lang to translate> <text> If you reply to message and you dont write text, userbot translate reply text, else userbot translate text in your command'''
+        args = args.split()
+        translator = Translator()
+        reply = await message.get_reply_message()
+        dest = 'язык'
+        try:
+            args[1]
+        except IndexError:
+            try:
+                args[0]
+            except IndexError:
+                dest = set_default_lang(self.db)
+            else:
+                dest = args[0]
+            if reply:
+                text = reply.text
+                translated_text = translator.translate(
+                    text=text, 
+                    dest=dest
+                )
+
+                await utils.answer(message,translated_text.text)
+        else:
+            try:
+                args[0]
+            except IndexError:
+                dest = set_default_lang(self.db)
+            else:
+                dest = args[0]
+            text = ' '.join(args[1:])
+
+            translated_text = translator.translate(
+                text=text, 
+                dest=dest
+            )
+
+            await utils.answer(message,translated_text.text)
+
